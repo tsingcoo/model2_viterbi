d99e1a8e9185ea0d792f3ae3330f29b7b2c2b545
wangql <wangql@nlp.nju.edu.cn>
2016-05-23 20:41:50 +0800

    Initial Commit

---
+++ model2_viterbi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  model2_viterbi
+//
+//  Created by 王青龙 on 5/23/16.
+//  Copyright © 2016 王青龙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
